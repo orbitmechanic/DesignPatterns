536ad4373f9b606d90566f48cc022c4c37edc2f1
orbit <orbitmechanic@protonmail.com>
2020-06-24 17:45:18 -0700

    Created Journal struct

---
+++ cpp/solid/SingleResponsibility/Journal/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+struct Journal
+{
+    string title;
+    vector<string> entries;
+};
+int main() {
+    getchar();
+    return 0;
+}
