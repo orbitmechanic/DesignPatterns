047d5e1529b3662f7b8b55c38578983a7beeadcd
orbit <orbitmechanic@protonmail.com>
2020-06-29 16:08:59 -0700

    Auto-generated Journal constructor that sets title now working w/ Boost libraries.

--- cpp/solid/SingleResponsibility/Journal/main.cpp
+++ cpp/solid/SingleResponsibility/Journal/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <cstdio>
 #include <string>
 #include <vector>
+#include <fstream>
+#include <boost/lexical_cast.hpp>
 using namespace std;
+using namespace boost;
 
 struct Journal
 {
@@ -9,8 +13,17 @@ struct Journal
     vector<string> entries;
 
     Journal(const string &title) : title(title) {}
+
+    void add_entry(const string& entry)
+    {
+        static int count = 1;
+        entries.push_back(lexical_cast<string>(count++) + ": " + entry);
+    }
+
 };
 int main() {
-    getchar();
+    Journal journal{"Dear Diary"};
+    journal.add_entry("I ate a bug");
+    journal.add_entry("I cried today");
     return 0;
 }
