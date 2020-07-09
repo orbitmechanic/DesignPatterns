52be59150e5dcfe54c44f374fdad8f035418a8ec
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-07-09 13:34:39 -0700

    Added dependency segregation example

---
+++ cpp/solid/5_DependencySegregation/Relationships/cmake-build-debug/CMakeFiles/Relationships.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Relationships.dir/main.cpp.obj"
+  "Relationships.exe"
+  "Relationships.exe.manifest"
+  "Relationships.lib"
+  "Relationships.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Relationships.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
