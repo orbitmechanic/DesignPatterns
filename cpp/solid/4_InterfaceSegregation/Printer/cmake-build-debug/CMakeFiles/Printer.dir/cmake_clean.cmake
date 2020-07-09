ce9e59c493ef7bc2f715815309c9752e4def8633
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-07-09 12:29:36 -0700

    Adding Segmentation example

---
+++ cpp/solid/4_InterfaceSegregation/Printer/cmake-build-debug/CMakeFiles/Printer.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Printer.dir/main.cpp.obj"
+  "Printer.exe"
+  "Printer.exe.manifest"
+  "Printer.lib"
+  "Printer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Printer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
