63234a6b2d3cbc52d94707ec862eb2c9b57ea757
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-07-10 15:20:17 -0700

    Solving Factory Exercise

---
+++ cpp/creational/2_Factory/5_Excersise02_PersonFactory/cmake-build-debug/CMakeFiles/5_Excersise02_PersonFactory.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "5_Excersise02_PersonFactory.exe"
+  "5_Excersise02_PersonFactory.exe.manifest"
+  "5_Excersise02_PersonFactory.lib"
+  "5_Excersise02_PersonFactory.pdb"
+  "CMakeFiles/5_Excersise02_PersonFactory.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/5_Excersise02_PersonFactory.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- cpp/creational/2_Factory/5_Excersise02_PersonFactory/main.cpp
+++ cpp/creational/2_Factory/5_Excersise02_PersonFactory/main.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
+#include <string>
+using namespace std;
+
+/* Factory Coding Exercise
+ * You are given a class called "Person".
+ * The "Person has two fields: id, and name.
+ */
+
+struct Person
+{
+    int id;
+    string name;
+};
+
+// Please implement a non-static PersonFactory
+class PersonFactory
+{
+    int32_t  population{0};
+public:
+    // That has a create_person() method that takes a person's name.
+    Person create_person(const string& name)
+    {
+        // The id of the person should be set as a
+        // 0-based index of the object created.
+        // So, the first person the factory makes should have id=0,
+        // Second id=1 and so on.
+        Person p;
+        p.id = population++;
+        p.name = name;
+        return p;
+    }
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    PersonFactory pf;
+    Person Bob = pf.create_person("Bob");
+    Person Sue = pf.create_person("Sue");
+    cout << "Bob's name is:" << Bob.name << " id:" << Bob.id << endl;
+    cout << "Sue's name is:" << Sue.name << " id:" << Sue.id << endl;
 }
