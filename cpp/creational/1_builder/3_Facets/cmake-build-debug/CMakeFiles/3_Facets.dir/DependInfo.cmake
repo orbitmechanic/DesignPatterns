295f0ac452d988d816db840979e245fa735404e7
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-07-10 04:51:20 -0700

    Adding Faceted Builder example

---
+++ cpp/creational/1_builder/3_Facets/cmake-build-debug/CMakeFiles/3_Facets.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/orbit/Documents/code/DesignPatterns/cpp/creational/1_builder/3_Facets/Facets.cpp" "C:/Users/orbit/Documents/code/DesignPatterns/cpp/creational/1_builder/3_Facets/cmake-build-debug/CMakeFiles/3_Facets.dir/Facets.cpp.obj"
+  "C:/Users/orbit/Documents/code/DesignPatterns/cpp/creational/1_builder/3_Facets/Person.cpp" "C:/Users/orbit/Documents/code/DesignPatterns/cpp/creational/1_builder/3_Facets/cmake-build-debug/CMakeFiles/3_Facets.dir/Person.cpp.obj"
+  "C:/Users/orbit/Documents/code/DesignPatterns/cpp/creational/1_builder/3_Facets/PersonBuilder.cpp" "C:/Users/orbit/Documents/code/DesignPatterns/cpp/creational/1_builder/3_Facets/cmake-build-debug/CMakeFiles/3_Facets.dir/PersonBuilder.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cpp/creational/1_builder/3_Facets/cmake-build-debug/CMakeFiles/3_Facets.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "3_Facets.exe"
+  "3_Facets.exe.manifest"
+  "3_Facets.lib"
+  "3_Facets.pdb"
+  "CMakeFiles/3_Facets.dir/Facets.cpp.obj"
+  "CMakeFiles/3_Facets.dir/Person.cpp.obj"
+  "CMakeFiles/3_Facets.dir/PersonBuilder.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3_Facets.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
