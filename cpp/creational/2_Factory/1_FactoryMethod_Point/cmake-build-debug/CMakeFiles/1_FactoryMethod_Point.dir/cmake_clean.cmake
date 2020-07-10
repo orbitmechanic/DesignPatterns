97025696135ec517b4d8b20880920e1ce24bba2a
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-07-10 12:43:50 -0700

    Adding Point Factory Method example

---
+++ cpp/creational/2_Factory/1_FactoryMethod_Point/cmake-build-debug/CMakeFiles/1_FactoryMethod_Point.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1_FactoryMethod_Point.exe"
+  "1_FactoryMethod_Point.exe.manifest"
+  "1_FactoryMethod_Point.lib"
+  "1_FactoryMethod_Point.pdb"
+  "CMakeFiles/1_FactoryMethod_Point.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1_FactoryMethod_Point.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
