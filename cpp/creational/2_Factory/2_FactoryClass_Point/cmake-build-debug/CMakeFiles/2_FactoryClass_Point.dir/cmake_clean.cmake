2ea6709f41748c1c4c7b54e6f4e90e62ddaff0ed
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-07-10 13:05:05 -0700

    Stubbing in Point Factory

---
+++ cpp/creational/2_Factory/2_FactoryClass_Point/cmake-build-debug/CMakeFiles/2_FactoryClass_Point.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2_FactoryClass_Point.exe"
+  "2_FactoryClass_Point.exe.manifest"
+  "2_FactoryClass_Point.lib"
+  "2_FactoryClass_Point.pdb"
+  "CMakeFiles/2_FactoryClass_Point.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2_FactoryClass_Point.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
