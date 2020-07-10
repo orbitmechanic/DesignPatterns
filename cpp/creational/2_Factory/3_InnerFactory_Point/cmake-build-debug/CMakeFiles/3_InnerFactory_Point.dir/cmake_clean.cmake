f7fd1ca46af17a718694de127a3b39c7c9d9954a
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-07-10 13:16:56 -0700

    Added Inner Factory example

---
+++ cpp/creational/2_Factory/3_InnerFactory_Point/cmake-build-debug/CMakeFiles/3_InnerFactory_Point.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "3_InnerFactory_Point.exe"
+  "3_InnerFactory_Point.exe.manifest"
+  "3_InnerFactory_Point.lib"
+  "3_InnerFactory_Point.pdb"
+  "CMakeFiles/3_InnerFactory_Point.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3_InnerFactory_Point.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
