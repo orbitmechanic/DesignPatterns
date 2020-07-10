03aa9cca54967ef6fac6bd9d667d31ab5891f535
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-07-10 13:47:52 -0700

    Addd abstract factory example

---
+++ cpp/creational/2_Factory/4_AbstractFactory_HotDrink/cmake-build-debug/CMakeFiles/4_AbstractFactory_HotDrink.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "4_AbstractFactory_HotDrink.exe"
+  "4_AbstractFactory_HotDrink.exe.manifest"
+  "4_AbstractFactory_HotDrink.lib"
+  "4_AbstractFactory_HotDrink.pdb"
+  "CMakeFiles/4_AbstractFactory_HotDrink.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/4_AbstractFactory_HotDrink.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- cpp/creational/2_Factory/4_AbstractFactory_HotDrink/main.cpp
+++ cpp/creational/2_Factory/4_AbstractFactory_HotDrink/main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <memory>
+#include <map>
+#include "HotDrink.h"
+#include "DrinkFactory.h"
+using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+unique_ptr<HotDrink> make_drink(string type)
+{
+    unique_ptr<HotDrink> drink;
+    if (type == "tea")
+    {
+        drink = make_unique<Tea>();
+        drink->prepare(200);
+    }
+    else
+    {
+        drink = make_unique<Coffee>();
+        drink->prepare(50);
+    }
+    return drink;
+}
+
+int main()
+{
+    auto d = make_drink("tea");
+
+    DrinkFactory df;
+    df.make_drink("coffee");
+
+    getchar();
     return 0;
 }
