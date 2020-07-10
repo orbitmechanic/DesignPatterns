c2b8bb22033d9856a5e1d1a420df65cc1041423a
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-07-10 08:02:17 -0700

    Finishing Builder Exercise

---
+++ cpp/creational/1_builder/4_Excercise01_CodeBuilder/cmake-build-debug/CMakeFiles/4_Excercise01_CodeBuilder.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "4_Excercise01_CodeBuilder.exe"
+  "4_Excercise01_CodeBuilder.exe.manifest"
+  "4_Excercise01_CodeBuilder.lib"
+  "4_Excercise01_CodeBuilder.pdb"
+  "CMakeFiles/4_Excercise01_CodeBuilder.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/4_Excercise01_CodeBuilder.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- cpp/creational/1_builder/4_Excercise01_CodeBuilder/main.cpp
+++ cpp/creational/1_builder/4_Excercise01_CodeBuilder/main.cpp
@@ -1,6 +1,60 @@
+#include <string>
+#include <vector>
+#include <ostream>
 #include <iostream>
+using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+struct Code{
+    std::string name;
+    std::vector<std::pair<std::string, std::string>> fields;
+    // constructors
+    Code() = default;
+
+    friend ostream &operator<<(ostream &os, const Code &code) {
+        os << "class " << code.name << endl;
+        os << '{' << endl;
+        for (const auto& f: code.fields)
+            os << "  " << f.first << ' ' << f.second << ';' << endl;
+        os << "};" << endl;
+        return os;
+    }
+};
+
+class CodeBuilder
+{
+
+
+public:
+    explicit CodeBuilder(const string& class_name)
+    {
+        root.name = class_name;
+    }
+
+    CodeBuilder& add_field(const string& name, const string& type)
+    {
+        root.fields.emplace_back(make_pair(type,name));
+        return *this;
+    }
+
+    friend ostream &operator<<(ostream &os, const CodeBuilder &builder) {
+        os << builder.root;
+        return os;
+    }
+
+    Code root;
+};
+
+int main(){
+    // This code:
+    auto cb = CodeBuilder{"Person"}.add_field("name","string").add_field("age","int");
+    cout << cb;
+
+    /* Should return:
+     * class Person
+     * {
+     *   string name;
+     *   int age;
+     * };
+     * Mind the curly brace placement and 2-space indentation
+    */
 }
