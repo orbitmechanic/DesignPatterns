b419a80bac173f5c01f2e3d62ce9f94b051b1e3c
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-07-11 14:42:02 -0700

    Adding Address Exaple- Trivial Case

---
+++ cpp/creational/3_Prototype/0_ContactTracing_vs_Addresses/cmake-build-debug/CMakeFiles/0_ContactTracing_vs_Addresses.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "0_ContactTracing_vs_Addresses.exe"
+  "0_ContactTracing_vs_Addresses.exe.manifest"
+  "0_ContactTracing_vs_Addresses.lib"
+  "0_ContactTracing_vs_Addresses.pdb"
+  "CMakeFiles/0_ContactTracing_vs_Addresses.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/0_ContactTracing_vs_Addresses.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- cpp/creational/3_Prototype/0_ContactTracing_vs_Addresses/main.cpp
+++ cpp/creational/3_Prototype/0_ContactTracing_vs_Addresses/main.cpp
@@ -1,6 +1,52 @@
+#define _USE_MATH_DEFINES
 #include <iostream>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <tuple>
+#include <sstream>
+#include <memory>
+#include <cmath>
+using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+struct Address
+{
+    string street, city;
+    int suite;
+
+    Address(const string &street,
+            const string &city, int suite) :
+            street(street), city(city), suite(suite) {}
+
+    friend ostream &operator<<(ostream &os, const Address &address) {
+        os << "street: " << address.street << " city: "
+        << address.city << " suite: " << address.suite;
+        return os;
+    }
+};
+
+struct Contact
+{
+    string name;
+    Address address;
+
+    Contact(const string &name, const Address &address) : name(name), address(address) {}
+
+    friend ostream &operator<<(ostream &os, const Contact &contact) {
+        os << "name: " << contact.name << " address: " << contact.address;
+        return os;
+    }
+};
+int main()
+{
+    Contact john{"John Doe", Address{"123 East Dr", "London", 123}};
+    // Duplicate code way:
+    // contact jane{"Jane Doe", Address{"123 East Dr", "London", 103}};
+    // More efficient:
+    Contact jane = john;
+    jane.name = "Jane Smith";
+    jane.address.suite = 103;
+
+    cout << john << endl << jane << endl;
 }
