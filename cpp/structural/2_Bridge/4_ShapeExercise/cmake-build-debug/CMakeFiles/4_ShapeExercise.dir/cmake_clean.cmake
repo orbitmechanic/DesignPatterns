8d94c3ba0f28353ca98b392b1df9e70f7af976a3
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-07-20 15:48:26 -0700

    Solving bridge exercise.

---
+++ cpp/structural/2_Bridge/4_ShapeExercise/cmake-build-debug/CMakeFiles/4_ShapeExercise.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "4_ShapeExercise.exe"
+  "4_ShapeExercise.exe.manifest"
+  "4_ShapeExercise.lib"
+  "4_ShapeExercise.pdb"
+  "CMakeFiles/4_ShapeExercise.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/4_ShapeExercise.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
