ef99db1c9a269eb6fd9825dcadb8cd093ef8acda
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-07-22 10:22:39 -0700

    Facade MMORG example
    Adding w/ Boost working.

---
+++ cpp/structural/6_Flyweight/1_MMORG/cmake-build-debug/CMakeFiles/1_MMORG.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1_MMORG.exe"
+  "1_MMORG.exe.manifest"
+  "1_MMORG.lib"
+  "1_MMORG.pdb"
+  "CMakeFiles/1_MMORG.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1_MMORG.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
