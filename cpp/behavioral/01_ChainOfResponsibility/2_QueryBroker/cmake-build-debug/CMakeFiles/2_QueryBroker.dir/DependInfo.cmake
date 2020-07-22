34362199d9273af3150257c707b2b5203746dca5
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-07-22 15:45:35 -0700

    Chain of Responsibilty: Queries
    Using a query listener system to manage a distributed chain of responsibility.

---
+++ cpp/behavioral/01_ChainOfResponsibility/2_QueryBroker/cmake-build-debug/CMakeFiles/2_QueryBroker.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/orbit/Documents/code/DesignPatterns/cpp/behavioral/01_ChainOfResponsibility/2_QueryBroker/main.cpp" "C:/Users/orbit/Documents/code/DesignPatterns/cpp/behavioral/01_ChainOfResponsibility/2_QueryBroker/cmake-build-debug/CMakeFiles/2_QueryBroker.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "C:/Program Files/boost/boost_1_73_0"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cpp/behavioral/01_ChainOfResponsibility/2_QueryBroker/cmake-build-debug/CMakeFiles/2_QueryBroker.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2_QueryBroker.exe"
+  "2_QueryBroker.exe.manifest"
+  "2_QueryBroker.lib"
+  "2_QueryBroker.pdb"
+  "CMakeFiles/2_QueryBroker.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2_QueryBroker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
