49fd7749390775d472f2b05ef9340c0daf93ede1
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-07-22 10:27:04 -0700

    Flyweight Text Formatting
    Adding text formatting example of flyweight pattern.

---
+++ cpp/structural/6_Flyweight/2_TextFormat/cmake-build-debug/CMakeFiles/2_TextFormat.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2_TextFormat.exe"
+  "2_TextFormat.exe.manifest"
+  "2_TextFormat.lib"
+  "2_TextFormat.pdb"
+  "CMakeFiles/2_TextFormat.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2_TextFormat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
