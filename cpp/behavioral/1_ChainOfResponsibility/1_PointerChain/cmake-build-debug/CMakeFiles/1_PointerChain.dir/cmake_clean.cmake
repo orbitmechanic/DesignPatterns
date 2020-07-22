5e81190648cb1bd53d2bae3bcd0434488be941df
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-07-22 15:36:38 -0700

    Chain of Responsibility: Pointers
    Using a singly-linked list to track a chain of responsibity in an MMO item modification scenario.

---
+++ cpp/behavioral/1_ChainOfResponsibility/1_PointerChain/cmake-build-debug/CMakeFiles/1_PointerChain.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1_PointerChain.exe"
+  "1_PointerChain.exe.manifest"
+  "1_PointerChain.lib"
+  "1_PointerChain.pdb"
+  "CMakeFiles/1_PointerChain.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1_PointerChain.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
