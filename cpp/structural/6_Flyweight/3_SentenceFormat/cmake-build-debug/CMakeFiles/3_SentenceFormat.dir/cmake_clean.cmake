5e3c8a088dc2a093776413bf11bc50950de7c35e
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-07-22 11:27:18 -0700

    Flyweight exercise
    You are given a class called "Sentence", which takes a string such as "hello world".   You need to provide an interface such that the indexer (operator[]) returns a WordToken that can be used to capitalize a particular word in the sentence.   Typical use would be something like: Sentence sentence ("hello world"); sentence[1].capitalize=true; cout<< sentence.str(); // prints "hello WORLD"

---
+++ cpp/structural/6_Flyweight/3_SentenceFormat/cmake-build-debug/CMakeFiles/3_SentenceFormat.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "3_SentenceFormat.exe"
+  "3_SentenceFormat.exe.manifest"
+  "3_SentenceFormat.lib"
+  "3_SentenceFormat.pdb"
+  "CMakeFiles/3_SentenceFormat.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3_SentenceFormat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
