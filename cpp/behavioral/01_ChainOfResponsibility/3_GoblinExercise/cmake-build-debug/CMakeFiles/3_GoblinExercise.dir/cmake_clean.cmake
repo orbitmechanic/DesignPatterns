5ac5c3a7732438dc6810578ed96f1c9918327db8
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-07-22 15:55:06 -0700

    Chain of Responsibility Exercise
    You are given a game scenario with classes "Goblin" and "GoblinKing.   Please implement the following rules:
    -A goblin has base 1 attack/1 defense(1/1) a goblin king is 3/3.
    -When the Goblin King is in play, every other goblin gets +1 Attack.
    -Goblins get +1 to Defense for every other Goblin in play (a GoblinKing is a Goblin!).

---
+++ cpp/behavioral/01_ChainOfResponsibility/3_GoblinExercise/cmake-build-debug/CMakeFiles/3_GoblinExercise.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "3_GoblinExercise.exe"
+  "3_GoblinExercise.exe.manifest"
+  "3_GoblinExercise.lib"
+  "3_GoblinExercise.pdb"
+  "CMakeFiles/3_GoblinExercise.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3_GoblinExercise.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
