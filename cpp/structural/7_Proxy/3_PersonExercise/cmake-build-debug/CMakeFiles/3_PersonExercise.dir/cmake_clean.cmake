1f64ca94773a5b4ade0270df9391e68cf966ea7e
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-07-22 13:53:21 -0700

    Responsible Person Proxy
    Proxy a Person.
    Allowed to drink over 18 yrs old.
    Allowed to drive over 16 yrs old.
    reports 'dead' if driving while drunk.

---
+++ cpp/structural/7_Proxy/3_PersonExercise/cmake-build-debug/CMakeFiles/3_PersonExercise.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "3_PersonExercise.exe"
+  "3_PersonExercise.exe.manifest"
+  "3_PersonExercise.lib"
+  "3_PersonExercise.pdb"
+  "CMakeFiles/3_PersonExercise.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3_PersonExercise.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
