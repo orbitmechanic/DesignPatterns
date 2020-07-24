0505a6014f9eb668306e5d59c3c6f26bfbb168e5
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-07-24 14:22:11 -0700

    Command example: bank account
    A basic bank account object to be acted upon.  Basic command object to report as acting.

---
+++ cpp/behavioral/02_Command/1_SimpleBankAccount/cmake-build-debug/CMakeFiles/1_SimpleBankAccount.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1_SimpleBankAccount.exe"
+  "1_SimpleBankAccount.exe.manifest"
+  "1_SimpleBankAccount.lib"
+  "1_SimpleBankAccount.pdb"
+  "CMakeFiles/1_SimpleBankAccount.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1_SimpleBankAccount.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
