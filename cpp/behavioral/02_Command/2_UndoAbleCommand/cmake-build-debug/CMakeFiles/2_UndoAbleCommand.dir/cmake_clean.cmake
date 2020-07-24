91fdef2981746d3d625af855ad003f060a17bf6f
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-07-24 14:26:49 -0700

    Adding undo to command example

---
+++ cpp/behavioral/02_Command/2_UndoAbleCommand/cmake-build-debug/CMakeFiles/2_UndoAbleCommand.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2_UndoAbleCommand.exe"
+  "2_UndoAbleCommand.exe.manifest"
+  "2_UndoAbleCommand.lib"
+  "2_UndoAbleCommand.pdb"
+  "CMakeFiles/2_UndoAbleCommand.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2_UndoAbleCommand.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
