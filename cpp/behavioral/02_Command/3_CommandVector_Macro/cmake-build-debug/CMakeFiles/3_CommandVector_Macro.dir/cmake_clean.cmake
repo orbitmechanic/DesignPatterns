27b84ceca69036985796da1cfa67a923644cc860
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-07-24 14:31:58 -0700

    Command vector: macro example.

---
+++ cpp/behavioral/02_Command/3_CommandVector_Macro/cmake-build-debug/CMakeFiles/3_CommandVector_Macro.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "3_CommandVector_Macro.exe"
+  "3_CommandVector_Macro.exe.manifest"
+  "3_CommandVector_Macro.lib"
+  "3_CommandVector_Macro.pdb"
+  "CMakeFiles/3_CommandVector_Macro.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3_CommandVector_Macro.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
