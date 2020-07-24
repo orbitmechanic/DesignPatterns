3087f68604ce9b28ac4e010f38f5d4a7b30c5585
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-07-24 14:36:32 -0700

    Completing command exercise

---
+++ cpp/behavioral/02_Command/4_CommandExercise/cmake-build-debug/CMakeFiles/4_CommandExercise.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "4_CommandExercise.exe"
+  "4_CommandExercise.exe.manifest"
+  "4_CommandExercise.lib"
+  "4_CommandExercise.pdb"
+  "CMakeFiles/4_CommandExercise.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/4_CommandExercise.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
