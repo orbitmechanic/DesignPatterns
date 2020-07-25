ab45a3c08822c6c736b966190b201d033908f081
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-07-25 11:03:02 -0700

    Solving Rat Observer Exercise

---
+++ cpp/behavioral/07_Observer/3_RatObserverExercise/cmake-build-debug/CMakeFiles/3_RatObserverExercise.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "3_RatObserverExercise.exe"
+  "3_RatObserverExercise.exe.manifest"
+  "3_RatObserverExercise.lib"
+  "3_RatObserverExercise.pdb"
+  "CMakeFiles/3_RatObserverExercise.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3_RatObserverExercise.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
