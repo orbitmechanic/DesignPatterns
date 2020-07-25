4411c34c172c79d6350cf848413aa288121e3b32
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-07-25 16:34:34 -0700

    Resolving template trading card game exercise

---
+++ cpp/behavioral/10_TemplateMethod/2_TradingCardGameExercise/cmake-build-debug/CMakeFiles/2_TradingCardGameExercise.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2_TradingCardGameExercise.exe"
+  "2_TradingCardGameExercise.exe.manifest"
+  "2_TradingCardGameExercise.lib"
+  "2_TradingCardGameExercise.pdb"
+  "CMakeFiles/2_TradingCardGameExercise.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2_TradingCardGameExercise.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
