30cb9f9785af707e7997a4f1397dd43bce99e780
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-07-24 20:36:57 -0700

    Iterator Facade example
    uses boost

---
+++ cpp/behavioral/04_Iterator/2_IteratorFacade/cmake-build-debug/CMakeFiles/2_IteratorFacade.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2_IteratorFacade.exe"
+  "2_IteratorFacade.exe.manifest"
+  "2_IteratorFacade.lib"
+  "2_IteratorFacade.pdb"
+  "CMakeFiles/2_IteratorFacade.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2_IteratorFacade.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
