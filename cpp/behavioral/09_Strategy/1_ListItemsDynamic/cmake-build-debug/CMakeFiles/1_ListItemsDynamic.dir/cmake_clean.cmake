989a2a989a5961ac7532175ca6d2523f3166bcf9
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-07-25 16:06:14 -0700

    Strategy Dynamic list item example

---
+++ cpp/behavioral/09_Strategy/1_ListItemsDynamic/cmake-build-debug/CMakeFiles/1_ListItemsDynamic.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1_ListItemsDynamic.exe"
+  "1_ListItemsDynamic.exe.manifest"
+  "1_ListItemsDynamic.lib"
+  "1_ListItemsDynamic.pdb"
+  "CMakeFiles/1_ListItemsDynamic.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1_ListItemsDynamic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
