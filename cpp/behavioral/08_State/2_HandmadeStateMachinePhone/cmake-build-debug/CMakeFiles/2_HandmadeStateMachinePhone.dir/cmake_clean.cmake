d9c898c83c5eb1e28ee610cb8b30fa8d76e71406
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-07-25 12:55:55 -0700

    State Machine hand-built for phone.

---
+++ cpp/behavioral/08_State/2_HandmadeStateMachinePhone/cmake-build-debug/CMakeFiles/2_HandmadeStateMachinePhone.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2_HandmadeStateMachinePhone.exe"
+  "2_HandmadeStateMachinePhone.exe.manifest"
+  "2_HandmadeStateMachinePhone.lib"
+  "2_HandmadeStateMachinePhone.pdb"
+  "CMakeFiles/2_HandmadeStateMachinePhone.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2_HandmadeStateMachinePhone.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
