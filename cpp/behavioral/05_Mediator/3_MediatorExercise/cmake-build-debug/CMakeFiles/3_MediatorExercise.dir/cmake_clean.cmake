32e6b3729fdacede01f1ea2909e9d69152cee6ae
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-07-24 22:18:25 -0700

    Solving mediator exercise

---
+++ cpp/behavioral/05_Mediator/3_MediatorExercise/cmake-build-debug/CMakeFiles/3_MediatorExercise.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "3_MediatorExercise.exe"
+  "3_MediatorExercise.exe.manifest"
+  "3_MediatorExercise.lib"
+  "3_MediatorExercise.pdb"
+  "CMakeFiles/3_MediatorExercise.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3_MediatorExercise.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
