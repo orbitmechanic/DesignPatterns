4e59f996d1c3f3584766de0aff22d6f1602d99d1
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-07-24 20:43:48 -0700

    Solving iterator exercise

---
+++ cpp/behavioral/04_Iterator/3_IteratorExercise/cmake-build-debug/CMakeFiles/3_IteratorExercise.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "3_IteratorExercise.exe"
+  "3_IteratorExercise.exe.manifest"
+  "3_IteratorExercise.lib"
+  "3_IteratorExercise.pdb"
+  "CMakeFiles/3_IteratorExercise.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3_IteratorExercise.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
