d76080087b384f224cc6831ee2dc0b7472c9d5be
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-07-25 08:15:14 -0700

    REsolving Momento Exercise

---
+++ cpp/behavioral/06_Memento/2_MomentoExercise/cmake-build-debug/CMakeFiles/2_MomentoExercise.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2_MomentoExercise.exe"
+  "2_MomentoExercise.exe.manifest"
+  "2_MomentoExercise.lib"
+  "2_MomentoExercise.pdb"
+  "CMakeFiles/2_MomentoExercise.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2_MomentoExercise.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
