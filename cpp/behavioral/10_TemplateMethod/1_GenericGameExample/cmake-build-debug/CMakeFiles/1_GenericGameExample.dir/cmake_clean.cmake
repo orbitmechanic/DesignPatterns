b8d900a8602c33a901936f5cdee60d00bce89aab
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-07-25 16:32:19 -0700

    Adding Template Game Example

---
+++ cpp/behavioral/10_TemplateMethod/1_GenericGameExample/cmake-build-debug/CMakeFiles/1_GenericGameExample.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1_GenericGameExample.exe"
+  "1_GenericGameExample.exe.manifest"
+  "1_GenericGameExample.lib"
+  "1_GenericGameExample.pdb"
+  "CMakeFiles/1_GenericGameExample.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1_GenericGameExample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
