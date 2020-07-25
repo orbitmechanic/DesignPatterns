f0cda2063e399af80081f2221c492700d28ec219
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-07-24 19:36:10 -0700

    Solving interpreter exercise

---
+++ cpp/behavioral/03_Interpreter/3_InterpreterExercise/cmake-build-debug/CMakeFiles/3_InterpreterExercise.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "3_InterpreterExercise.exe"
+  "3_InterpreterExercise.exe.manifest"
+  "3_InterpreterExercise.lib"
+  "3_InterpreterExercise.pdb"
+  "CMakeFiles/3_InterpreterExercise.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3_InterpreterExercise.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
