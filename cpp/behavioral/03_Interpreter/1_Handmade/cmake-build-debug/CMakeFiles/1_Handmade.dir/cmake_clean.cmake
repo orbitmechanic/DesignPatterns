c4aa077379d7987299cd2e203e533ec7b2731120
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-07-24 19:15:57 -0700

    Handmade interpreter
    Does use Boost lexical_cast

---
+++ cpp/behavioral/03_Interpreter/1_Handmade/cmake-build-debug/CMakeFiles/1_Handmade.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1_Handmade.exe"
+  "1_Handmade.exe.manifest"
+  "1_Handmade.lib"
+  "1_Handmade.pdb"
+  "CMakeFiles/1_Handmade.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1_Handmade.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
