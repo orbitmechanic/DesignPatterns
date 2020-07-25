9bcdb124066922fb916a23c2fba38fe2492700ad
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-07-24 22:10:43 -0700

    Mediator Sports game event example.

---
+++ cpp/behavioral/05_Mediator/2_SportsGameEvents/cmake-build-debug/CMakeFiles/2_SportsGameEvents.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2_SportsGameEvents.exe"
+  "2_SportsGameEvents.exe.manifest"
+  "2_SportsGameEvents.lib"
+  "2_SportsGameEvents.pdb"
+  "CMakeFiles/2_SportsGameEvents.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2_SportsGameEvents.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
