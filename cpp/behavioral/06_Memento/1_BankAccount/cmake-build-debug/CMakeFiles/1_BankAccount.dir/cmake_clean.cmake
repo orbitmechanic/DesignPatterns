6ff29862c8f4edd7ab9e0dbe291a6e62687d70af
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-07-25 08:10:12 -0700

    Momento bank account example

---
+++ cpp/behavioral/06_Memento/1_BankAccount/cmake-build-debug/CMakeFiles/1_BankAccount.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1_BankAccount.exe"
+  "1_BankAccount.exe.manifest"
+  "1_BankAccount.lib"
+  "1_BankAccount.pdb"
+  "CMakeFiles/1_BankAccount.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1_BankAccount.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
