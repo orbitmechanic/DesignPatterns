3c6e7165db371dc5f39ef38deb0dd785ff49d93b
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-07-25 12:59:01 -0700

    Adding Boost MSM State phone

---
+++ cpp/behavioral/08_State/3_BoostMSMPhone/cmake-build-debug/CMakeFiles/3_BoostMSMPhone.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "3_BoostMSMPhone.exe"
+  "3_BoostMSMPhone.exe.manifest"
+  "3_BoostMSMPhone.lib"
+  "3_BoostMSMPhone.pdb"
+  "CMakeFiles/3_BoostMSMPhone.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3_BoostMSMPhone.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
