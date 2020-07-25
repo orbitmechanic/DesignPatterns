cf40e9d04f0a6b763d8814a440f84667a702bf1c
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-07-25 12:51:25 -0700

    Classic Light Switch State Machine
    Obsolte.  How not to do it.

---
+++ cpp/behavioral/08_State/1_ClassicLightSwitch/cmake-build-debug/CMakeFiles/1_ClassicLightSwitch.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1_ClassicLightSwitch.exe"
+  "1_ClassicLightSwitch.exe.manifest"
+  "1_ClassicLightSwitch.lib"
+  "1_ClassicLightSwitch.pdb"
+  "CMakeFiles/1_ClassicLightSwitch.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1_ClassicLightSwitch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
