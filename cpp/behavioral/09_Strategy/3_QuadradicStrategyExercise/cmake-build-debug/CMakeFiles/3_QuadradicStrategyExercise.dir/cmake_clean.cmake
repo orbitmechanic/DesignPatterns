ebf0e991859b9e5acd3c11a874d890878b907c83
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-07-25 16:16:28 -0700

    Resolving Strategy Quadratic exercise

---
+++ cpp/behavioral/09_Strategy/3_QuadradicStrategyExercise/cmake-build-debug/CMakeFiles/3_QuadradicStrategyExercise.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "3_QuadradicStrategyExercise.exe"
+  "3_QuadradicStrategyExercise.exe.manifest"
+  "3_QuadradicStrategyExercise.lib"
+  "3_QuadradicStrategyExercise.pdb"
+  "CMakeFiles/3_QuadradicStrategyExercise.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3_QuadradicStrategyExercise.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
