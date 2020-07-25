8e2993b403b48d61357f2477b9be423a1f44cfc8
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-07-24 20:33:06 -0700

    Iterator for binary tree w/ stdlib
    Uses <experimental/coroutines> that compiler doesn't like.

---
+++ cpp/behavioral/04_Iterator/1_BinaryTree_stdlib/cmake-build-debug/CMakeFiles/1_BinaryTree_stdlib.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1_BinaryTree_stdlib.exe"
+  "1_BinaryTree_stdlib.exe.manifest"
+  "1_BinaryTree_stdlib.lib"
+  "1_BinaryTree_stdlib.pdb"
+  "CMakeFiles/1_BinaryTree_stdlib.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1_BinaryTree_stdlib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
