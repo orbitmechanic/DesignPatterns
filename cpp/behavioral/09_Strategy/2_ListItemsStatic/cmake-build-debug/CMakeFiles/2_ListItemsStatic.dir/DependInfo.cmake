39d945b0914d86f71315a010888f6e0ee1677808
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-07-25 16:07:52 -0700

    Strategy static list item example

---
+++ cpp/behavioral/09_Strategy/2_ListItemsStatic/cmake-build-debug/CMakeFiles/2_ListItemsStatic.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/orbit/Documents/code/DesignPatterns/cpp/behavioral/09_Strategy/2_ListItemsStatic/main.cpp" "C:/Users/orbit/Documents/code/DesignPatterns/cpp/behavioral/09_Strategy/2_ListItemsStatic/cmake-build-debug/CMakeFiles/2_ListItemsStatic.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cpp/behavioral/09_Strategy/2_ListItemsStatic/cmake-build-debug/CMakeFiles/2_ListItemsStatic.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2_ListItemsStatic.exe"
+  "2_ListItemsStatic.exe.manifest"
+  "2_ListItemsStatic.lib"
+  "2_ListItemsStatic.pdb"
+  "CMakeFiles/2_ListItemsStatic.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2_ListItemsStatic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
