6de831cb6e6a0f68586ef6b0c8232701b4e109a6
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-07-26 10:56:35 -0700

    Adding reflective visitor

---
+++ cpp/behavioral/11_Visitor/2_ReflectiveExpressionVisitor/cmake-build-debug/CMakeFiles/2_ReflectiveExpressionVisitor.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2_ReflectiveExpressionVisitor.exe"
+  "2_ReflectiveExpressionVisitor.exe.manifest"
+  "2_ReflectiveExpressionVisitor.lib"
+  "2_ReflectiveExpressionVisitor.pdb"
+  "CMakeFiles/2_ReflectiveExpressionVisitor.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2_ReflectiveExpressionVisitor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
