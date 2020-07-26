0834eaf5d1ae42ca5352c77e82e0a6b6221b107f
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-07-26 11:22:15 -0700

    Completing Expression Visitor Exercise

---
+++ cpp/behavioral/11_Visitor/7_ExpressionVisitorExercise/cmake-build-debug/CMakeFiles/7_ExpressionVisitorExercise.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "7_ExpressionVisitorExercise.exe"
+  "7_ExpressionVisitorExercise.exe.manifest"
+  "7_ExpressionVisitorExercise.lib"
+  "7_ExpressionVisitorExercise.pdb"
+  "CMakeFiles/7_ExpressionVisitorExercise.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/7_ExpressionVisitorExercise.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
