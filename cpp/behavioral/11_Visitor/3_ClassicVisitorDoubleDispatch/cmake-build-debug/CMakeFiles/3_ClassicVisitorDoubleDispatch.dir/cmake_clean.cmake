17fe90621f9efd6931ba75e6131665ce15afb319
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-07-26 11:01:25 -0700

    Classic Visitor (Double Dispatch)

---
+++ cpp/behavioral/11_Visitor/3_ClassicVisitorDoubleDispatch/cmake-build-debug/CMakeFiles/3_ClassicVisitorDoubleDispatch.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "3_ClassicVisitorDoubleDispatch.exe"
+  "3_ClassicVisitorDoubleDispatch.exe.manifest"
+  "3_ClassicVisitorDoubleDispatch.lib"
+  "3_ClassicVisitorDoubleDispatch.pdb"
+  "CMakeFiles/3_ClassicVisitorDoubleDispatch.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3_ClassicVisitorDoubleDispatch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
