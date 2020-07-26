7866e0e0ee1b0f3a7fd709fc1fae09d6c4d5df8e
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-07-26 11:10:24 -0700

    Multimethod Visitor Game Example

---
+++ cpp/behavioral/11_Visitor/5_MultimethodVisitor/cmake-build-debug/CMakeFiles/5_MultimethodVisitor.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "5_MultimethodVisitor.exe"
+  "5_MultimethodVisitor.exe.manifest"
+  "5_MultimethodVisitor.lib"
+  "5_MultimethodVisitor.pdb"
+  "CMakeFiles/5_MultimethodVisitor.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/5_MultimethodVisitor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
