dc1ecee7e96e5b89624765d03da2d75bcfd44fed
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-07-26 10:43:25 -0700

    Adding GoF Expression Visitor example

---
+++ cpp/behavioral/11_Visitor/1_ExpressionTraverseGo4/cmake-build-debug/CMakeFiles/1_ExpressionTraverseGo4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1_ExpressionTraverseGo4.exe"
+  "1_ExpressionTraverseGo4.exe.manifest"
+  "1_ExpressionTraverseGo4.lib"
+  "1_ExpressionTraverseGo4.pdb"
+  "CMakeFiles/1_ExpressionTraverseGo4.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1_ExpressionTraverseGo4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
