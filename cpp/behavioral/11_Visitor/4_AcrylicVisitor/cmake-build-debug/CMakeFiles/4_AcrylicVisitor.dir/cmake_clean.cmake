f1f9023b822f56f49bb40e7b91df76fc37deaa9e
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-07-26 11:04:55 -0700

    Adding Acrylic Visitor Example.

---
+++ cpp/behavioral/11_Visitor/4_AcrylicVisitor/cmake-build-debug/CMakeFiles/4_AcrylicVisitor.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "4_AcrylicVisitor.exe"
+  "4_AcrylicVisitor.exe.manifest"
+  "4_AcrylicVisitor.lib"
+  "4_AcrylicVisitor.pdb"
+  "CMakeFiles/4_AcrylicVisitor.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/4_AcrylicVisitor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
