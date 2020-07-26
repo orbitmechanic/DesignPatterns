8345df2fb5cf2cac2e61eafa7913ade723b9e804
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-07-26 11:16:34 -0700

    Using std::variant to create a visitor

---
+++ cpp/behavioral/11_Visitor/6_AddressBookSTDVariant/cmake-build-debug/CMakeFiles/6_AddressBookSTDVariant.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "6_AddressBookSTDVariant.exe"
+  "6_AddressBookSTDVariant.exe.manifest"
+  "6_AddressBookSTDVariant.lib"
+  "6_AddressBookSTDVariant.pdb"
+  "CMakeFiles/6_AddressBookSTDVariant.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/6_AddressBookSTDVariant.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
